db20e69b3684e4dcb759eb2d17bb3b8623083c0b
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-06-05 07:10:24 -0500

    passwordless

--- vagrant/cluster/scripts/passwordless.sh
+++ vagrant/cluster/scripts/passwordless.sh
@@ -44,13 +44,15 @@ function share_ssh_public_key(){
   local host_address=$2
   local host_password=$3
 
+  echo "host_username: $host_username, host_address: $host_address, host_password: $host_password"
+
   # Checking if the mpi ssh key already exists
   if [ -f "${HOME}/.ssh/id_rsa" ]
   then
     echo "Sharing the public key with $host_username@$host_address"
     write_log "Sharing the public key with $host_address"
     # Sharing the public key with the remote slave
-    sshpass -p "$host_password" ssh-copy-id -i "${HOME}/.ssh/id_rsa" "$host_username@$host_address"
+    sshpass -p "'$host_password'" ssh-copy-id -i "${HOME}/.ssh/id_rsa" "$host_username@$host_address"
   else
     echo "You dont have ssh keys to share." >&2
     write_log "You dont have ssh keys to share."
@@ -59,6 +61,6 @@ function share_ssh_public_key(){
 
 # MAIN
 setting_up_ssh_keys 'vagrant'
-share_ssh_public_key 'vagrant' 10.0.0.2 'vagrant'
-share_ssh_public_key 'vagrant' 10.0.0.3 'vagrant'
-share_ssh_public_key 'vagrant' 10.0.0.4 'vagrant'
+share_ssh_public_key 'vagrant' '10.0.0.2' 'vagrant'
+share_ssh_public_key 'vagrant' '10.0.0.3' 'vagrant'
+share_ssh_public_key 'vagrant' '10.0.0.4' 'vagrant'
